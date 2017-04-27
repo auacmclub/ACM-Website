bdd76a062d4cf62a415db21b88c63450bc243194
Shelby <shelbyleanne22@gmail.com>
2017-04-27 13:59:06 -0400

    Added Controllers and View Folders for each page

---
+++ Source/ACM-Website/Controllers/ProgrammingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACM_Website.Controllers
+{
+    public class ProgrammingController : Controller
+    {
+        // GET: Programming
+        // ActionResult returns HTML
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Resources()
+        {
+            return View();
+        }
+        public ActionResult Agenda()
+        {
+            return View();
+        }
+        public ActionResult Event()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return Info();
+        }
+    }
+}
